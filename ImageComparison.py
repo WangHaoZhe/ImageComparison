54daf2ad120c457d7b5bffc59bb5a6b096ed720a
Albert Wang <AlbertWang@github.com>
2020-06-20 22:23:27 +0800

    Converted the absolute path to the relative path

--- ImageComparison.py
+++ ImageComparison.py
@@ -4,9 +4,9 @@ import math
 import operator
 from functools import reduce
 
-path_one = r'C:\Users\13521\Desktop\ImageComparison\data\original\1.jpg'  # 原图片
-path_two = r'C:\Users\13521\Desktop\ImageComparison\data\changed\2.jpg'  # 输入图片
-diff_save_location = r'C:\Users\13521\Desktop\ImageComparison\data\difference\difference.jpg'  # 差异
+path_one = r'data\original\1.jpg'  # 原图片
+path_two = r'data\changed\2.jpg'  # 输入图片
+diff_save_location = r'data\difference\difference.jpg'  # 差异
 
 
 def compare_images(image1, image2, difference):
